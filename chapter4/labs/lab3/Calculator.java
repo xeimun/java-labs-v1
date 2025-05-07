17e6ca85305b59df172528c677d8f1d37ea69057
xeimun <hinote444@naver.com>
2025-05-07 17:07:28 +0900

    study: Chapter 4 실습 문제 풀이
    lab3 풀이 완료

--- chapter4/labs/lab3/Calculator.java
+++ chapter4/labs/lab3/Calculator.java
@@ -2,38 +2,59 @@ package chapter4.labs.lab3;
 
 /**
  * Lab 3: static과 final 키워드 사용하기
- * 
+ * <p>
  * Calculator 클래스 정의하기
  */
 public class Calculator {
     // TODO: PI 값을 나타내는 static final 상수를 정의하세요.
-    
-    
+    public static final double PI = 3.14159265359;
+
     // TODO: 인스턴스 변수를 정의하세요. (계산기 모델명, 계산 횟수 등)
-    
-    
+    public String calculatorName;
+    public int calculateCnt;
+
     // TODO: 계산기 객체의 총 개수를 나타내는 static 변수를 정의하세요.
-    
-    
+    public static int calculatorCnt = 0;
+
     // TODO: 생성자를 정의하고, 객체가 생성될 때마다 계산기 개수를 증가시키세요.
-    
-    
+    public Calculator() {
+        this.calculatorName = "daiso";
+        this.calculateCnt = 0;
+        calculatorCnt++;
+    }
+
     // TODO: 원의 면적을 계산하는 static 메소드를 정의하세요.
-    
-    
+    public static double calculateCircleArea(double radius) {
+        return PI * radius * radius;
+    }
+
     // TODO: 원의 둘레를 계산하는 static 메소드를 정의하세요.
-    
-    
+    public static double calculateCircumference(double radius) {
+        return 2 * PI * radius;
+    }
+
     // TODO: 사각형의 면적을 계산하는 static 메소드를 정의하세요.
-    
-    
+    public static double calculateArea(double x, double y) {
+        return x * y;
+    }
+
     // TODO: 인스턴스 메소드를 정의하고, 해당 메소드가 호출될 때마다 계산 횟수를 증가시키세요.
-    
-    
+    public void addCalculateCnt() {
+        this.calculateCnt++;
+    }
+
+    public int getCalculateCnt() {
+        return calculateCnt;
+    }
+
     // TODO: 계산기의 정보를 출력하는 메소드를 정의하세요.
-    
-    
+    public void getCalculateInfo() {
+        System.out.println("계산기 이름: " + this.calculatorName);
+        System.out.println("계산 횟수: " + this.calculateCnt);
+    }
+
     // TODO: 현재까지 생성된 계산기의 총 개수를 반환하는 static 메소드를 정의하세요.
-    
-    
-} 
+    public static int getCalculatorCnt() {
+        return calculatorCnt;
+    }
+}
--- chapter4/labs/lab3/FinalExample.java
+++ chapter4/labs/lab3/FinalExample.java
@@ -2,47 +2,78 @@ package chapter4.labs.lab3;
 
 /**
  * Lab 3: static과 final 키워드 사용하기
- * 
+ * <p>
  * final 키워드 예제 클래스
  */
 public class FinalExample {
     // TODO: final 변수를 선언하고 초기화하세요.
-    
-    
+    private final int finalInt = 0;
+
     // TODO: final 변수를 선언만 하고, 생성자에서 초기화하세요.
-    
-    
+    private final int finalInt2;
+
     // TODO: 생성자를 정의하세요.
-    public FinalExample() {
-        // final 변수 초기화
-        
+    public FinalExample(int num) {
+        this.finalInt2 = num;
     }
-    
+
     // TODO: final 메소드를 정의하세요.
-    
-    
+    public final int getFinalInt() {
+        return finalInt;
+    }
+
     // TODO: final 매개변수를 받는 메소드를 정의하세요.
-    
-    
+    public final void printInput(final int param) {
+        // param = 0; // 에러: final 매개변수는 값 변경 불가
+        System.out.println(param);
+    }
+
     public static void main(String[] args) {
         System.out.println("Final 키워드 예제를 실행합니다.");
-        
+
         // TODO: FinalExample 객체를 생성하세요.
-        
-        
+        FinalExample finalExample = new FinalExample(3);
+
         // TODO: 객체의 메소드를 호출하세요.
-        
-        
+        System.out.println(finalExample.getFinalInt());
+        finalExample.printInput(4);
+
         // TODO: final 클래스의 객체를 생성하고 메소드를 호출하세요.
-        
-        
+        FinalExample2 finalExample2 = new FinalExample2();
+        finalExample2.display();
+
         // TODO: final 지역 변수를 선언하고 사용하세요.
-        
-        
+        final int example3 = 6;
+        System.out.println(example3);
     }
 }
 
 // TODO: final 클래스를 정의하세요.
+final class FinalExample2 {
+    public void display() {
+        System.out.println("이 클래스는 상속할 수 없습니다.");
+    }
+}
 
+// TODO: 일반 클래스와 하위 클래스를 정의하고, final 메소드 오버라이딩을 시도해보세요.
+class Parent {
+    public final void cannotOverride() {
+        System.out.println("이 메소드는 오버라이딩 할 수 없습니다.");
+    }
 
-// TODO: 일반 클래스와 하위 클래스를 정의하고, final 메소드 오버라이딩을 시도해보세요. 
+    public void canOverride() {
+        System.out.println("이 메소드는 오버라이딩 가능합니다.");
+    }
+}
+
+class Child extends Parent {
+    // final 메소드 오버라이딩 시도(에러: final 메소드는 오버라이딩 불가)
+    // @Override
+    // public void cannotOverride(){
+    // }
+
+    @Override
+    public void canOverride() {
+        System.out.println("메소드를 오버라이딩했습니다.");
+    }
+}
